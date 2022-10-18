8de53f8fb1073bab02d62046b0649bcadb4c6a77
signm4 <signm3@gmail.com>
2022-10-18 02:23:07 -0500

    added getPoster Function

--- project1.py
+++ project1.py
@@ -8,10 +8,10 @@ def randomMovieGen():
     movieList =["Tron", "Fast and Furious", "IronMan"]
     movieList_key = [20526, 13804, 1726]
 
-    print("here is a favorite movie: ")
+    # print("here is a favorite movie: ")
 
     randomSelect = random.choice(movieList)
-    print(randomSelect)
+    #  print(randomSelect)
     index = movieList.index(randomSelect)
     return (movieList_key[index])
 
@@ -33,40 +33,35 @@ def pullMovieData():
     json_data = response.json()
     return json_data
 
+def getPoster(chosenMovie):
+    movie_data = chosenMovie
+    return (movie_data["poster_path"])
 
-def getGenre():
+def getGenre(chosenMovie):
+    movie_data = chosenMovie
+    # print("\nthe Genres are: ")
     
-    movie_data = pullMovieData()
-
-    print("\nthe Genres are: ")
     for i in movie_data["genres"]:
-        print (i["name"])
-
-def getTitle():
-
-    movie_data = pullMovieData()
-    print("\nThe offical title is : " + (movie_data["original_title"]))
-
-def getTagline():
-    movie_data = pullMovieData()
-    print("the movie tagline is: " + (movie_data["tagline"]))
+        return (i["name"] + " ")
 
 
+def getTitle(chosenMovie):
+    movie_data = chosenMovie
+    return((movie_data["original_title"]))
 
-#def main():
+def getTagline(chosenMovie):
+    movie_data = chosenMovie
+    return((movie_data["tagline"]))
 
-# #USING API
-#     #using dot env file and pulling key from .env file
-#     load_dotenv()
-#     api_key = os.getenv('TMDB_API_KEY')
 
-#     #made base URL, requests.get will pull the movie details, also need api key to access
-#     TMDB_MOVIE_API_REQUEST = 'https://api.themoviedb.org/3'
-#     rand_movie_id = randomMovieGen() # calls generator, generator returns movie_id
-#     movie_id = str(rand_movie_id)
-#     response =  requests.get(
-#         f"{TMDB_MOVIE_API_REQUEST}/movie/{movie_id}",
-#         params={'api_key' : api_key }
-#         )
 
-getTagline()
+def main():
+    chosenMovie = pullMovieData() # so it only generates the poster once
+    print ("\nThe title of the movie is: " + getTitle(chosenMovie))
+    print ("\nThe Genre is: " + getGenre(chosenMovie))
+    print("\nThe Tagline is: " + getTagline(chosenMovie))
+    print("\nHere is the poster: " + getPoster(chosenMovie))
+
+#getGenre(pullMovieData())
+
+main()
--- readme.md
+++ readme.md
@@ -3,5 +3,8 @@ What are at least 2 technical issues you encountered with your project? How did 
     taking in a input but then making sure it was a integer,
     to debug it i checked that i am taking in a input, then had to see if it was a integer, i thought i was doing it correctly as what i remember you should be doing int(variable), but apparently you have to do variable = int(variable), found this on stackoverflow
 
+    genres_str[i] = (i["name"] + " ")
+    TypeError: list indices must be integers or slices, not dict
+
 
 b. What are at least 2 known problems (still existing), if any, with your project? (If none, what are two things you would improve about your project if given more time?)
